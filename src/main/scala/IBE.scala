9b313ef43ff59f49ea2cd08a6bd6e79aec993227
t-bast <bastuc@hotmail.fr>
2019-04-09 20:52:32 +0200

    Extract private keys from user IDs. Absolutely no security guarantee until the hash to point algorithm is vetted.

--- src/main/scala/IBE.scala
+++ src/main/scala/IBE.scala
@@ -1,7 +1,8 @@
 import it.unisa.dia.gas.plaf.jpbc.pairing.a.TypeACurveGenerator
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory
+import it.unisa.dia.gas.jpbc.Element
 import java.math.BigInteger
-import java.security.SecureRandom
+import java.security.{MessageDigest, SecureRandom}
 
 /**
   * Encapsulate the IBE primitives.
@@ -13,6 +14,7 @@ case class IBE() {
 
   // Note: this is probably very insecure because IBE seems to require a type B pairing
   // but jPBC doesn't offer it...
+  // We also should use a specific, well-chosen curve instead of generating a random one.
   private val pairing = PairingFactory.getPairing(new TypeACurveGenerator(rBits, qBits).generate())
 
   // G1 and G2 both represent the elliptic curve group in a Type A pairing (symmetric).
@@ -26,13 +28,30 @@ case class IBE() {
     * Master public key.
     * TODO: this is currently insecure: the base point should be of order q.
     */
-  val P = curve.newRandomElement.mul(new BigInteger(masterKey))
+  val P: Element = curve.newRandomElement.mul(new BigInteger(masterKey)).getImmutable
+
+  /**
+    * Map an arbitrary string to a curve point.
+    * Note that we are *not* using the paper's recommended algorithm so this is
+    * most likely very insecure.
+    *
+    * @param id that should be mapped to a curve point.
+    * @return curve point.
+    */
+  private def mapToPoint(id: String): Element = {
+    val sha = MessageDigest.getInstance("SHA-256")
+    val h = sha.digest(id.getBytes())
+    curve.newElementFromHash(h, 0, h.length).getImmutable
+  }
 
   /**
     * Extract a private key for the given ID.
     *
-    * @param id of the user's key.
-    * @return TODO
+    * @param id of the user.
+    * @return this user's private key (curve point).
     */
-  def extract(id: String) = ???
+  def extract(id: String): Element = {
+    val q = mapToPoint(id)
+    q.mul(new BigInteger(masterKey)).getImmutable
+  }
 }
--- src/test/scala/IBESpec.scala
+++ src/test/scala/IBESpec.scala
@@ -2,10 +2,29 @@ import org.scalatest.{Matchers, WordSpec}
 
 class IBESpec extends WordSpec with Matchers {
   "Identity-Based Encryption" should {
+    val ibe = new IBE
+
     "initialize non-degenerate system parameters" in {
-      val ibe = new IBE
       ibe.P.isOne should ===(false)
       ibe.P.isZero should ===(false)
+    }
+
+    "extract non-degenerate user keys" in {
+      val sk = ibe.extract("eliot@ecorp.com")
+      sk.isOne should ===(false)
+      sk.isZero should ===(false)
+    }
+
+    "extract deterministic user keys" in {
+      val sk1 = ibe.extract("eliot@ecorp.com")
+      val sk2 = ibe.extract("eliot@ecorp.com")
+      sk1 should ===(sk2)
+    }
+
+    "extract private keys from user ids" in {
+      val sk1 = ibe.extract("eliot@ecorp.com")
+      val sk2 = ibe.extract("eliot@ecorp.com|2019")
+      sk1 should !==(sk2)
     }
   }
 }
